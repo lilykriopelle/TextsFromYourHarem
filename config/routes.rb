b5e2e22a9355757b0df3fa3ab2241f4553e5c6ae
Joseph Combs <joseph.e.combs@gmail.com>
2015-01-27 11:25:52 -0800

    some bare view stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'users/omniauth_callbacks' }
   
+  root 'plans#index'
+  
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
   
-  resources :plans, only: [:index, :show, :create, :update, :destroy, :new]
+  resources :plans, only: [:index, :show, :create, :update, :destroy, :new ]
 end
